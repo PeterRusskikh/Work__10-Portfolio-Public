20a0b5de93c05bccf25f838110b10fd2a98f50ed
PeterRusskikh <Trad696969@gmail.com>
2022-09-07 20:27:12 +0300

    1

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,23 @@
+
+/* Gallery----------------------------------------------------- */
+const hamburger = document.querySelector(".hamburger");
+const list = document.querySelector(".list");
+const soc = document.querySelector(".soc");
+
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	list.classList.toggle("active");
+	soc.classList.toggle("active");
+})
+
+document.querySelectorAll(".list-link").forEach(n => n.addEventListener("click", () => {
+	hamburger.classList.remove("active")
+	list.classList.remove("active")
+	soc.classList.remove("active")
+}))
+
+
+
 let position = 0;
 const slidesToShow = 3;
 const slidesToScroll = 1;
